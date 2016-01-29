08bca6f3921942b70e3063f9e444be4b4ee78bbe
Yacine Rezgui <rezgui.y@gmail.com>
2016-01-29 22:28:08 +0900

    Add STT Manager native module

--- ios/SpeechToTextManager.swift
+++ ios/SpeechToTextManager.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+import WatsonDeveloperCloud
+
+@objc(SpeechToTextManager)
+class SpeechToTextManager: NSObject {
+
+  var sttService:SpeechToText!
+  var recorder:AVAudioRecorder!
+  var audioPlayer:AVAudioPlayer!
+  
+  @objc func startService(username: String, password: String) -> Void {
+    NSLog("%@ %@", username, password);
+    self.sttService = SpeechToText(username: username, password: password)
+  }
+  
+  @objc func startRecording(callback: RCTResponseSenderBlock) -> Void {
+    NSLog("recording...")
+    
+    let filePath    = NSURL(fileURLWithPath: "\(NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0])/SpeechToTextRecording.wav")
+    
+    let session = AVAudioSession.sharedInstance()
+    var settings = [String: AnyObject]()
+    
+    settings[AVSampleRateKey] = NSNumber(float: 44100.0)
+    settings[AVNumberOfChannelsKey] = NSNumber(int: 1)
+    do {
+      try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+      self.recorder = try AVAudioRecorder(URL: filePath, settings: settings)
+    } catch {
+      // error
+    }
+    
+    callback([self.recorder.record()])
+  }
+  
+  @objc func stopRecording() -> Void {
+    NSLog("stop recording...")
+    self.recorder.stop()
+  }
+  
+  @objc func play() -> Void {
+    NSLog("stop recording...")
+    
+    let filePath    = NSURL(fileURLWithPath: "\(NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0])/SpeechToTextRecording.wav")
+    
+    do {
+      try self.audioPlayer = AVAudioPlayer(contentsOfURL: filePath)
+      self.audioPlayer.prepareToPlay()
+      self.audioPlayer.play()
+    } catch {
+      // error
+    }
+  }
+  
+  @objc func transcript(callback: RCTResponseSenderBlock) -> Void {
+    NSLog("transcripting...")
+    
+    let data = NSData(contentsOfURL: self.recorder.url)
+    
+    if let data = data {
+      
+      self.sttService.transcribe(data , format: .WAV, completionHandler: {response, error -> Void in
+        callback([(response?.transcription())!])
+      })
+    }
+  }
+}
--- ios/SpeechToTextManagerBridge.m
+++ ios/SpeechToTextManagerBridge.m
@@ -7,3 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+
+@interface RCT_EXTERN_MODULE(SpeechToTextManager, NSObject)
+
+RCT_EXTERN_METHOD(startService:(NSString *)username password:(NSString *)password)
+
+RCT_EXTERN_METHOD(startRecording:(RCTResponseSenderBlock)callback)
+
+RCT_EXTERN_METHOD(stopRecording)
+
+RCT_EXTERN_METHOD(play)
+
+RCT_EXTERN_METHOD(transcript:(RCTResponseSenderBlock)callback)
+
+@end

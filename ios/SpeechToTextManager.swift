28b184cadb94189676b22d8b38f89626a28a1432
Yacine Rezgui <rezgui.y@gmail.com>
2016-01-29 22:26:25 +0900

    Add files for SpeechToTextManager native module

---
+++ ios/SpeechToTextManager.swift
@@ -0,0 +1,9 @@
+//
+//  SpeechToTextManager.swift
+//  Seona
+//
+//  Created by Yacine Rezgui on 28/01/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ ios/SpeechToTextManagerBridge.m
@@ -0,0 +1,9 @@
+//
+//  SpeechToTextManagerBridge.m
+//  Seona
+//
+//  Created by Yacine Rezgui on 28/01/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
